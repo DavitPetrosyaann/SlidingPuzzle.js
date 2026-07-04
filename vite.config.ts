0c678c0effed77d01832af241ce0db871a434b93
DeimeralGIT <vahanhekimyan99@gmail.com>
2026-07-04 23:55:07 +0400

    deploy

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import path from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig(() => {
+  return {
+    base: '/games/sliding-puzzle/',
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, '.'),
+      }
+    },
+    build: {
+      outDir: 'dist',
+      emptyOutDir: true,
+    }
+  };
+});
